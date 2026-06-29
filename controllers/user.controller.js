08b9c3c07fa755496d5e51de777316903a62c15c
Elfetnimourad <moradtao2000@gmail.com>
2026-06-29 22:53:26 +0100

    user-controller done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,9 +25,38 @@ password:hashedPassword,
 role,
 
 })
+const token = jwt.sign({username:newUser.username,email:newUser.email,password:newUser.password,role:newUser.role},"77b4b5afdebcb8322d2ad2fc62188aaaff95e895549ba033cd4d80d4c2589ac4")
 
+newUser.token = token;
+await newUser.save();
 }
 
 
 //login
 
+const login = async(req,res)=>{
+    const {email,password} = req.body;
+const user = await User.findOne({email});
+if(!user){
+    return res.status(404).json("You Have To Sign")
+}
+
+if(!email && !password){
+    return res.status(404).json("Email & password are not Valid")
+}
+const comparedPassword = await bcrypt.compare(password,user.password);
+try{
+if(comparedPassword && user){
+    const token = jwt.sign({username:user.username,email:user.email,password:user.password,role:user.role},"77b4b5afdebcb8322d2ad2fc62188aaaff95e895549ba033cd4d80d4c2589ac4")
+user.token = token;
+return res.status(201).json({user})
+}
+}catch(error){
+return res.status(404).json(error)
+}
+
+}
+module.exports = {
+    register,
+    login
+}
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
+app.use(cors())
 
+mongoose.connect("mongodb+srv://moradtao2000_db_user:moradelf1234@cluster0.dzddk8y.mongodb.net/mini-ecom?appName=Cluster0"
+).then(()=>{
+    console.log("DB IS READY")
+})
 
-
-
-app.listen("7000",()=>{
+app.listen("7000",()=>{ 
     console.log("Listenning on port 7000")
 })
